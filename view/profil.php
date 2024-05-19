b1bfeda181fa9d6264f0f99046f7bef68f2a374c
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-19 14:45:50 +0200

    ajout du bouton back-office

--- view/profil.php
+++ view/profil.php
@@ -3,9 +3,13 @@ $title = 'Mon Profil';
 require_once 'header.php'; // Inclusion de l'en-tête
 
 ?>
+    
 <div class="container">
     <a href="<?= SERVER_URL ?>/deconnexion/" class="btn btn-danger">Déconnexion</a> <!-- Bouton de déconnexion -->
 </div>
+<?php if($_SESSION['idRole'] == 1) {?>
+    <a href="<?= SERVER_URL ?>/deconnexion/" class="btn btn-danger">Back-office</a> 
+<?php } ?>
 
 
 <?php
