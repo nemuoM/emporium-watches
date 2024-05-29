41359764ffee7e4046f503309be5239c306c09e6
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-30 00:20:00 +0200

    chore: Supprimer les fichiers de classes inutilisées

--- autoload.php
+++ autoload.php
@@ -8,24 +8,6 @@
  * 11/2023
  */
 
- require_once ROOT.'/model/class_tempo/Accessoires.php';
- require_once ROOT.'/model/class_tempo/Categorie.php';
- require_once ROOT.'/model/class_tempo/Roles.php';
- require_once ROOT.'/model/class_tempo/Clients.php';
- require_once ROOT.'/model/class_tempo/Transporteur.php';
- require_once ROOT.'/model/class_tempo/Commandes.php';
- require_once ROOT.'/model/class_tempo/Couleur.php';
- require_once ROOT.'/model/class_tempo/Genre.php';
- require_once ROOT.'/model/class_tempo/Marque.php';
- require_once ROOT.'/model/class_tempo/Marque.php';
- require_once ROOT.'/model/class_tempo/Matiere.php';
- require_once ROOT.'/model/class_tempo/Mouvement.php';
- require_once ROOT.'/model/class_tempo/Style.php';
- require_once ROOT.'/model/class_tempo/Montre.php';
- require_once ROOT.'/model/class_tempo/Details_accessoires.php';
- require_once ROOT.'/model/class_tempo/Details_montre.php';
- require_once ROOT.'/model/class_tempo/Avis.php';
- require_once ROOT.'/model/class_tempo/Ss_categorie.php';
 
  require_once ROOT.'/model/DbManager.php';
  require_once ROOT.'/model/ProductManager.php';
