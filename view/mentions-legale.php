6a68439c77373e0eb70e4a0795e7235927548795
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-30 00:17:46 +0200

    chore: Ajouter la page "Mentions légales" et les styles associés

---
+++ view/mentions-legale.php
@@ -0,0 +1,30 @@
+<?php
+
+$title = 'Mentions légales'; // Titre de la page
+require_once 'header.php'; // Inclusion de l'en-tête
+
+?>
+
+<div class="container" style="margin-top: 75px; margin-bottom: 75px">
+    <h1>Mentions légales</h1>
+    <p>Bienvenue sur Emporium Watches (ci-après dénommé "le Site"). En accédant et en utilisant ce Site, vous acceptez les présentes mentions légales. Si vous n'acceptez pas ces conditions, nous vous demandons de ne pas utiliser notre Site.</p>
+    <p>Ces mentions légales sont susceptibles d'être modifiées ou complétées à tout moment, sans préavis. Nous vous conseillons donc de les consulter régulièrement.</p>
+        
+    <h2>Gestion des cookies</h2>
+    <p>Notre Site utilise des cookies pour améliorer votre expérience utilisateur. Les cookies sont de petits fichiers texte enregistrés sur votre appareil lorsque vous visitez notre Site. Ils nous aident à comprendre comment vous utilisez notre Site et nous permettent de personnaliser votre expérience.</p>
+    <p>En continuant à naviguer sur ce Site, vous acceptez notre utilisation des cookies. Vous pouvez à tout moment modifier les paramètres de votre navigateur pour refuser les cookies ou être informé de leur utilisation. Cependant, veuillez noter que certaines fonctionnalités de notre Site pourraient ne pas fonctionner correctement sans l'utilisation de cookies.</p>
+    <p>Pour plus d'informations sur les cookies que nous utilisons et comment nous les utilisons, veuillez consulter notre politique de cookies.</p>
+
+    <h2>Respect du RGPD</h2>
+    <p>Nous respectons le Règlement Général sur la Protection des Données (RGPD) et nous nous engageons à protéger vos données personnelles. Nous collectons et traitons vos données de manière transparente, sécurisée et dans le respect de vos droits.</p>
+    <p>Pour plus d'informations sur la collecte et l'utilisation de vos données, veuillez consulter notre politique de confidentialité. Vous y trouverez des détails sur les types de données que nous collectons, les finalités de leur traitement, ainsi que vos droits en matière de protection des données et comment les exercer.</p>
+    <p>Si vous avez des questions concernant notre politique de confidentialité ou la gestion de vos données personnelles, vous pouvez nous contacter à l'adresse suivante : [votre adresse email de contact].</p>
+        
+    <p>En utilisant notre Site, vous consentez à notre politique de confidentialité et à notre utilisation des cookies conformément à cette politique. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser notre Site.</p>
+</div>
+
+<?php
+
+require_once 'footer.php'; // Inclusion du pied de page
+
+?>
