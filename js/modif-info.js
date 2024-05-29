01dbce3c66b2cdae737ba65458274567f3e3cbed
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-30 00:16:30 +0200

    chore: Update modif-info.js to handle form submission for updating profile information and deleting account

--- js/modif-info.js
+++ js/modif-info.js
@@ -1,20 +1,44 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("info").addEventListener("submit", function (e) {
-        e.preventDefault();
-        let form = new FormData(this);
+document.getElementById("info").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let form = new FormData(this);
 
-        fetch("updtprofil/", {
-            method: "POST",
-            body: form,
+    fetch("updtprofil/", {
+        method: "POST",
+        body: form,
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.success) {
+                alert(data.success);
+                location.reload();
+            } else {
+                alert(data.error);
+            }
+        });
+});
+
+$("#deltebtn").click(function () {
+    let form = new FormData();
+    form.append("password", $("#password").val());
+    fetch("supprimer/", {
+        method: "POST",
+        body: form,
+    })
+        .then((response) => {
+            if (!response.ok) {
+                console.log("HTTP status code: " + response.status);
+                throw new Error("The request failed");
+            }
+            return response.json();
         })
-            .then((response) => response.json())
-            .then((data) => {
-                if (data.success) {
-                    alert(data.success);
-                    location.reload();
-                } else {
-                    alert(data.error);
-                }
-            });
-    });
+        .then((data) => {
+            if (data.error === "Mot de passe incorrect !") {
+                alert(data.error);
+            } else {
+                window.location.href = "../../../../";
+            }
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
 });
