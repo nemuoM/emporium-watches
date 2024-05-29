eb6e187c65579ee75c839c38d0db338506be3664
nemuoM <abdelmoumen.morabet@gmail.com>
2024-05-30 00:17:42 +0200

    chore: Add "Les meilleurs ventes" page template and styles

---
+++ view/meilleurs-ventes.php
@@ -0,0 +1,40 @@
+<?php
+
+$title = 'Les meilleurs ventes'; // Titre de la page
+require_once 'header.php'; // Inclusion de l'en-tête
+
+?>
+
+<link rel="stylesheet" href="<?= SERVER_URL ?>/css/style-pdt.css" />
+
+<div class="modal" id="errorModal">
+  <div class="modal-content">
+    <p id="errorMessage"></p>
+  </div>
+</div>
+
+<div class="container center pdts mx-auto">
+    <h1 class="pagespe">Les nouveaux arrivants</h1>
+    <div class="affichepdt" id="mesProd">
+        <!-- Affichage de mes produits -->
+    </div>
+</div>
+
+<script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"
+></script>
+<?php if(isset($_SESSION['idClient'])) {?>
+<script src="<?= SERVER_URL ?>/js/afficher-produits-connected.js"></script>
+<?php } else {?>
+<script src="<?= SERVER_URL ?>/js/afficher-produits-disconnected.js"></script>
+<?php } ?>
+    <script src="<?= SERVER_URL ?>/js/addToCart.js"></script>
+
+
+<?php
+
+require_once 'footer.php'; // Inclusion du pied de page
+
+?>
